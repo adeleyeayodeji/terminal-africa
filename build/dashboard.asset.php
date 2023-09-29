43611b7adce07273259bdb7454db4bfb9b5af085
Adeleye Ayodeji <biodunhi@gmail.com>
2023-09-29 11:57:45 +0100

    Added address book

--- build/dashboard.asset.php
+++ build/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element'), 'version' => 'e13d322a580a1597b930');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element'), 'version' => '141ca129735dbe9c61fb');
