2c37554bb594448f042728527ec4f79357f4eb31
Adeleye Ayodeji <biodunhi@gmail.com>
2023-11-03 11:27:51 +0100

    Added update

--- templates/settings.php
+++ templates/settings.php
@@ -14,9 +14,66 @@ $terminal_custom_price_mark_up = get_option('terminal_custom_price_mark_up', '')
     <div class="t-body" style="padding-top: 10px;">
         <div class="t-row">
             <div class="t-col-12">
-                <?php
-                echo getTerminalTemplatePart('settings', ['countries' => $countries, 'saved_currency' => $saved_currency, 'terminal_custom_price_mark_up' => $terminal_custom_price_mark_up]);
-                ?>
+                <div class="t-address-card t-settings-page">
+                    <h3>Customise your Shipping Experience for your WooCommerce store.</h3>
+
+                    <div class="t-flex t-settings-page-card t-mb-4">
+                        <div>
+                            <p class="t-settings-page-card-title">
+                                Show Rates
+                            </p>
+                            <p class="t-settings-page-card-description">
+                                When enabled, your customer can see your rates.
+                            </p>
+                        </div>
+                        <div>
+                            <div class="t-carrier-embed w-embed">
+                                <label class="t-switch t-carrier-switch">
+                                    <input type="checkbox" class="t-carrier-checkbox">
+                                    <span class="t-slider round"></span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="t-flex t-settings-page-card t-mb-4">
+                        <div>
+                            <p class="t-settings-page-card-title">
+                                Show Rates
+                            </p>
+                            <p class="t-settings-page-card-description">
+                                When enabled, your customer can see your rates.
+                            </p>
+                        </div>
+                        <div>
+                            <div class="t-carrier-embed w-embed">
+                                <label class="t-switch t-carrier-switch">
+                                    <input type="checkbox" class="t-carrier-checkbox">
+                                    <span class="t-slider round"></span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="t-flex t-settings-page-card t-mb-4">
+                        <div>
+                            <p class="t-settings-page-card-title">
+                                Show Rates
+                            </p>
+                            <p class="t-settings-page-card-description">
+                                When enabled, your customer can see your rates.
+                            </p>
+                        </div>
+                        <div>
+                            <div class="t-carrier-embed w-embed">
+                                <label class="t-switch t-carrier-switch">
+                                    <input type="checkbox" class="t-carrier-checkbox">
+                                    <span class="t-slider round"></span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
