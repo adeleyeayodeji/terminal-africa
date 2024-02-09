830c01721c3d2693fdc9dd4e5eee630f6e43f6e1
adeleyeayodeji <biodunhi@gmail.com>
2024-02-09 05:15:41 +0100

    Update version in dashboard.asset.php

--- build/dashboard.asset.php
+++ build/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element'), 'version' => '2b843c2c9fab7418defe');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element'), 'version' => '82601b47bac4b5359865');
